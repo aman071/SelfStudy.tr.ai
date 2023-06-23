e82044f9ecd403c993078f383074d2eeb822ed06
aman071 <amanrehman18@gmail.com>
2023-06-23 23:04:11 +0530

    added dark mode(incomplete), bb exists

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import './App.css';
 import NavBar from './Components/NavBar';
 import Banner from './Components/Banner';
 import TextForm from './Components/TextForm';
+import { useState } from 'react';
 
 
 function App() {
+  const [mode, setMode] = useState('light');  //we want to handle entire page's darkmode, so we set a state in App.js
+
+  const toggleMode = () => {
+    if(mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor='black';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor='white';
+    }
+  }
+
   return (
     <>
-      <NavBar title="SelfStudy.tr.ai"/>
+      <NavBar title="SelfStudy.tr.ai" mode={mode} toggleMode={toggleMode}/>
       <Banner/>
       <div className="container my-3">
         <TextForm heading="What did you learn today?"/>
