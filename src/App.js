d83c75b4556915ebe4934b9aa848231de43c34b0
aman071 <amanrehman18@gmail.com>
2023-06-26 00:46:18 +0530

    alert added, bb exists

--- src/App.js
+++ src/App.js
@@ -3,26 +3,42 @@ import './App.css';
 import NavBar from './Components/NavBar';
 import Banner from './Components/Banner';
 import TextForm from './Components/TextForm';
+import Alert from './Components/Alert';
 import { useState } from 'react';
 
 
 function App() {
   const [mode, setMode] = useState('light');  //we want to handle entire page's darkmode, so we set a state in App.js
+  const [alert, setAlert] = useState(null);  //we want to handle entire page's darkmode, so we set a state in App.js
+
+  const showAlert = (message, type) => {
+    setAlert({
+      message:message,
+      type:type
+    })
+
+    setTimeout(() => {
+      setAlert(null)
+    }, 2000);
+  }
 
   const toggleMode = () => {
     if(mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor='#495057';
+      showAlert('Dark mode enabled.', 'success')
     }
     else {
       setMode('light');
       document.body.style.backgroundColor='white';
+      showAlert('Dark mode disabled.', 'success')
     }
   }
 
   return (
     <>
       <NavBar title="SelfStudy.tr.ai" mode={mode} toggleMode={toggleMode}/>
+      <Alert alert={alert} />
       <Banner/>
       <div className="container my-3">
         <TextForm heading="What did you learn today?" mode={mode} />
---
+++ src/Components/Alert.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Alert(props) {
+
+  const capitalize = (word) => {
+    let lower = word.toLowerCase();
+    return word.charAt(0).toUpperCase() + lower.slice(1);
+  }
+
+  return (
+    // NOTICE THIS SYNTAX. The second expression returns true always since it is a div element and just shows content, but it will only
+    // run if props.alert is true, which means it is not NULL.
+    //alert-primary is blue,    alert-success is green
+    //alert-warning is yellow,  alert-danger is red
+    props.alert && <div class={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+        <strong>{capitalize(props.alert.type)}</strong>: {props.alert.message}
+        {/*Below is the X button on dismissble alert. Removing it cuz we are going to auto-dismiss alert through timer.*/}
+        {/* <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>  */} 
+    </div>
+  )
+}
+
+export default Alert
