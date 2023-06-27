70f89d39204d704a61f2b08cbed655034298111d
aman071 <amanrehman18@gmail.com>
2023-06-27 19:07:09 +0530

    banner fix in progress, visible

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -3,10 +3,32 @@ import React from 'react'
 export default function Banner() {
   return (
     <>
-        <div id="carousel" className="carousel slide">
+      <div id="carouselExampleFade" className="carousel slide carousel-fade">
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img src={process.env.PUBLIC_URL + '/study_banner.jpg'} style={{ height: '800px', width: 'auto' }} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={process.env.PUBLIC_URL + '/study_banner.jpg'} style={{ height: '800px', width: 'auto' }} className="d-block w-100" alt="..." />
+          </div>
+          <div className="carousel-item">
+            <img src={process.env.PUBLIC_URL + '/study_banner.jpg'} style={{ height: '800px', width: 'auto' }} className="d-block w-100" alt="..." />
+          </div>
+        </div>
+        <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+          <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+          <span className="carousel-control-next-icon" aria-hidden="true"></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    
+        {/* <div id="carousel" className="carousel slide">
         <div className="carousel-inner">
             <div className="carousel-item active">
-            <img src="..\public\study_banner.jpg" className="d-block w-100" alt="Simple notebook image" />
+            <img src={process.env.PUBLIC_URL + '/study_banner.jpg'} style={{ height: '800px', width: 'auto' }} className="d-block w-100" alt="Simple notebook" />
             </div>
         </div>
         {/* <button className="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
@@ -16,8 +38,8 @@ export default function Banner() {
         <button className="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
             <span className="carousel-control-next-icon" aria-hidden="true"></span>
             <span className="visually-hidden">Next</span>
-        </button> */}
-        </div>
+        </button> 
+        </div> */}
     </>
   )
 }
